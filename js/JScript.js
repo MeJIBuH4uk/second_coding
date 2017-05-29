404897969ce859321fc02a7d537c5573b9763f6a
green <green@orlov.com.ua>
2017-05-29 03:57:07 +0300

    header done, nav done, logo-section done

---
+++ js/JScript.js
@@ -0,0 +1,159 @@
+$('#nav1').click("on", function() {
+    $('#nav1').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "silver");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav2').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "silver");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav3').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "silver");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav4').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "silver");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav5').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "silver");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav6').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "silver");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav7').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+    $('#nav8').css("border-bottom", "none");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "silver");
+    $('#nav8').css("color", "black");
+});
+
+$('#nav8').click("on", function() {
+    $('#nav1').css("border-bottom", "none");
+    $('#nav2').css("border-bottom", "none");
+    $('#nav3').css("border-bottom", "none");
+    $('#nav4').css("border-bottom", "none");
+    $('#nav5').css("border-bottom", "none");
+    $('#nav6').css("border-bottom", "none");
+    $('#nav7').css("border-bottom", "none");
+    $('#nav8').css("border-bottom", "5px solid rgba(0, 102, 255, 0.69)");
+
+    $('#nav1').css("color", "black");
+    $('#nav2').css("color", "black");
+    $('#nav3').css("color", "black");
+    $('#nav4').css("color", "black");
+    $('#nav5').css("color", "black");
+    $('#nav6').css("color", "black");
+    $('#nav7').css("color", "black");
+    $('#nav8').css("color", "black");
+});
